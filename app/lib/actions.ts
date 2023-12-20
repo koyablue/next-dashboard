1cb345bf5ebd164c861328524592ff04a743004d
koyaaoyama <aoyama.ko0920@gmail.com>
2023-12-19 22:52:08 -0800

    Done chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,22 +7,42 @@ import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.'
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = formSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+export type State = {
+  errors?: {
+    customerId?: string[]
+    amount?: string[]
+    status?: string[]
+  };
+  message?: string | null
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
 
-  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Field to Create Invoice.ss',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100; // To avoid JS floating-point errors
   const date = new Date().toISOString().split('T')[0]; // '2023-12-18T04:37:34.867Z' -> '2023-12-18'
 
@@ -36,7 +56,6 @@ export async function createInvoice(formData: FormData) {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
-
 
   revalidatePath('/dashboard/invoices'); // revalidate cache and fetch latest data
   redirect('/dashboard/invoices')
